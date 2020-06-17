718c282b35049c7e7be99ac004136e5b6317d4e1
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-06-17 21:34:04 +0700

    jwt implementation

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 const PORT = process.env.PORT || 8888
+const users = [
+  { id: 1, username: 'admin', password: 'admin' },
+  { id: 2, username: 'guest', password: 'guest' }
+]
 
 app.use(bodyParser.json())
 
-app.get('/status', (req, res) => {
-  const localTime = (new Date()).toLocaleTimeString()
-
-  res
-    .status(200)
-    .send(`Server time is ${localTime}.`)
-})
-
 app.post('/login', (req, res) => {
-  const username = req.body.username
+  if (!req.body.username || !req.body.password) {
+    res
+      .status(400)
+      .send('You nee a username and password!')
+
+    return
+  }
+
+  const user = users.find((u) => {
+    return u.username === req.body.username && u.password === req.body.password
+  })
+
+  if (!user) {
+    res
+      .status(401)
+      .send('User not found')
+
+    return
+  }
+
+  const token = jwt.sign({
+    sub: user.id,
+    username: user.username
+  }, "mysupersecretkey", { expiresIn: "3 hours" })
 
   res
     .status(200)
-    .send(`You logged in with ${username}.`)
+    .send({
+      access_token: token
+    })
 })
 
 app.get('*', (req, res) => {
