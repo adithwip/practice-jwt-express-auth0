9a3983468ed3b467013c22f93a5fa8051152a365
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-06-17 21:06:33 +0700

    try body parser

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const PORT = process.env.PORT || 8888
+
+app.use(bodyParser.json())
+
+app.get('/status', (req, res) => {
+  const localTime = (new Date()).toLocaleTimeString()
+
+  res
+    .status(200)
+    .send(`Server time is ${localTime}.`)
+})
+
+app.post('/login', (req, res) => {
+  const username = req.body.username
+
+  res
+    .status(200)
+    .send(`You logged in with ${username}.`)
+})
+
+app.get('*', (req, res) => {
+  res.sendStatus(404)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`)
+})
