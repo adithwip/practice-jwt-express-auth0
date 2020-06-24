63160a434c980a66a73c6f7511ce2cf4f7353e54
System Administrator <root@Adiths-MacBook-Pro.local>
2020-06-24 08:58:39 +0700

    add lesson7 folder

---
+++ lesson7/index.js
@@ -0,0 +1,28 @@
+/* To run this file on a server, we are using httpster. 
+Type `httpster index.html -p 5000` in your console to start the server. */
+
+const API_URL = "";
+const AUTH_URL = "";
+
+let ACCESS_TOKEN;
+
+const headlineBtn = document.querySelector("#headline");
+const secretBtn = document.querySelector("#secret");
+const loginBtn = document.querySelector("#loginBtn");
+const logoutBtn = document.querySelector("#logoutBtn");
+
+headlineBtn.addEventListener("click", () => {
+
+});
+
+secretBtn.addEventListener("click", (event) => {
+
+});
+
+logoutBtn.addEventListener("click", (event) => {
+
+});
+
+loginBtn.addEventListener("click", (event) => {
+
+});
